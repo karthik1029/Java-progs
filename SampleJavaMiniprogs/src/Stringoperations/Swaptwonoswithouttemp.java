f182881199399b936f03cb4ae51618c79f8d6fe4
karthik <karthik@karthik-PC>
2016-10-10 18:09:31 -0500

    modified program

--- SampleJavaMiniprogs/src/Stringoperations/Swaptwonoswithouttemp.java
+++ SampleJavaMiniprogs/src/Stringoperations/Swaptwonoswithouttemp.java
@@ -5,9 +5,12 @@ public class Swaptwonoswithouttemp {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		a=a+b;
-		b=a-b;
-		a=a-b;
+		//a=a+b;
+		a=a^b;
+		//b=a-b;
+		b=a^b;
+		//a=a-b;
+		a=a^b;
 		System.out.println(a+"  "+b); 
 
 	}
