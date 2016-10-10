d5c04109e316a07ff930c5171747fe87702415c5
karthik <karthik@karthik-PC>
2016-10-10 16:58:48 -0500

    modified factorial

--- SampleJavaMiniprogs/src/Stringoperations/Factorial.java
+++ SampleJavaMiniprogs/src/Stringoperations/Factorial.java
@@ -3,6 +3,15 @@ package Stringoperations;
 import java.util.Scanner;
 
 public class Factorial {
+	
+	public int fact(int num)
+	{
+		if(num==0)
+			return 1;
+		
+		else
+			return num *fact(num-1);
+	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -16,6 +25,9 @@ public class Factorial {
 		}
 		System.out.println("The Factorial is: "+result);
 		sc.close();
+		
+		Factorial f=new Factorial(); 
+		System.out.println("Factorial using recursion: "+f.fact(5));
 	}
 
 }
