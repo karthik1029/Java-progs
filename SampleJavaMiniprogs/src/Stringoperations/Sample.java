c1aae4c2fb1f0c1854670389cebbc43107cb526f
karthik <karthik@karthik-PC>
2016-11-28 19:35:58 -0600

    added a program

--- SampleJavaMiniprogs/src/Stringoperations/Sample.java
+++ SampleJavaMiniprogs/src/Stringoperations/Sample.java
@@ -2,11 +2,10 @@ package Stringoperations;
 
 
 public class Sample {
+  
+    
+
    public static void main(String[] args) {
-	  String s="aaabba";
-	  String s1=s.toUpperCase();
-	  System.out.println(s1);
 	  
    }
 }
- 
