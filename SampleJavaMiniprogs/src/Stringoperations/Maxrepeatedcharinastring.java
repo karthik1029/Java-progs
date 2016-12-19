df94fb5090dbb628b87c13d6808152dedeeb54db
karthik <karthik@karthik-PC>
2016-12-18 21:43:26 -0600

    added a program

--- SampleJavaMiniprogs/src/Stringoperations/Maxrepeatedcharinastring.java
+++ SampleJavaMiniprogs/src/Stringoperations/Maxrepeatedcharinastring.java
@@ -10,8 +10,9 @@ public class Maxrepeatedcharinastring {
 		int count=0,max=0;
 		for(int i=0;i<n;i++)
 		{
+			
 			count=0;
-			for(int j=i;j<n;j++)
+			for(int j=0;j<n;j++)
 			{
 				if(ch[i]==ch[j])
 					count++;
