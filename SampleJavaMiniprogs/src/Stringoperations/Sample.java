0fdcced79c1224a64d4c55cb80b4272b2b334d1e
karthik <karthik@karthik-PC>
2017-01-09 10:22:13 -0600

    added program

--- SampleJavaMiniprogs/src/Stringoperations/Sample.java
+++ SampleJavaMiniprogs/src/Stringoperations/Sample.java
@@ -1,31 +1,16 @@
 package Stringoperations;
 
 public class Sample {
+	
 
-	public static void main(String[] args)  {
-		
-		String s="geegf";
-		char c=0;
-		int count;
-		char[] ch=s.toCharArray();
-		for(int i=0;i<ch.length;i++)
-		{
-			count=0;
-			for(int j=0;j<ch.length;j++)
-			{
-				if(ch[i]==ch[j])
-				{
-					count++;
-				}
-			}
-			if(count==1)
-			{
-				
-				c=ch[i];
-				break;
-			}
-		}
-		
-		System.out.println(c);
-	}
+public static void main(String[] args)  {
+	
+
+
 }
+
+}
+
+
+
+	
