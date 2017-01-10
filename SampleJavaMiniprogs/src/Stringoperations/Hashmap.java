e217ed174ae58a291972e1b7d26b2d408a8995b4
karthik <karthik@172.20.21.57>
2017-01-09 23:08:03 -0600

    added programs

--- SampleJavaMiniprogs/src/Stringoperations/Hashmap.java
+++ SampleJavaMiniprogs/src/Stringoperations/Hashmap.java
@@ -1,8 +1,39 @@
 package Stringoperations;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
 public class Hashmap {
 
 	public static void main(String[] args) {
+		
+HashMap<String, Integer> hm=new HashMap<String, Integer>();
+		
+		hm.put("kc", 1);
+		hm.put("rc", 2);
+		
+		@SuppressWarnings("rawtypes")
+		Set e=hm.entrySet();
+		
+		@SuppressWarnings("rawtypes")
+		Iterator i=e.iterator();
+		
+		while(i.hasNext())
+		{
+			@SuppressWarnings("rawtypes")
+			Map.Entry me=(Entry) i.next();
+			System.out.print(me.getKey()+" ");
+			System.out.println(me.getValue());
+		}
+		System.out.println();
+		
+		int x=hm.get("kc");
+		hm.put("kc", x+1000);
+		System.out.println("kc's balance: "+hm.get("kc"));
+
 		
 		
 
--- SampleJavaMiniprogs/src/Stringoperations/Sample.java
+++ SampleJavaMiniprogs/src/Stringoperations/Sample.java
@@ -1,11 +1,14 @@
 package Stringoperations;
 
+
+
 public class Sample {
 	
 
+
 public static void main(String[] args)  {
 	
-
+System.out.println(3&10);
 
 }
 
